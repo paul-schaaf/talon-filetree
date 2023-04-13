6d3ec2438ffacaf65be47fa0ce02f04a96134ecb
Paul Schaaf <paulsimonschaaf@gmail.com>
2023-04-14 00:01:29 +0200

    cleanup

--- src/fileExplorer.ts
+++ src/fileExplorer.ts
@@ -1,148 +1,7 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-import * as mkdirp from 'mkdirp';
-import { rimrafSync } from 'rimraf';
-
-//#region Utilities
-
-namespace _ {
-
-	function handleResult<T>(resolve: (result: T) => void, reject: (error: Error) => void, error: Error | null | undefined, result: T): void {
-		if (error) {
-			reject(massageError(error));
-		} else {
-			resolve(result);
-		}
-	}
-
-	function massageError(error: Error & { code?: string }): Error {
-		if (error.code === 'ENOENT') {
-			return vscode.FileSystemError.FileNotFound();
-		}
-
-		if (error.code === 'EISDIR') {
-			return vscode.FileSystemError.FileIsADirectory();
-		}
-
-		if (error.code === 'EEXIST') {
-			return vscode.FileSystemError.FileExists();
-		}
-
-		if (error.code === 'EPERM' || error.code === 'EACCESS') {
-			return vscode.FileSystemError.NoPermissions();
-		}
-
-		return error;
-	}
-
-	export function checkCancellation(token: vscode.CancellationToken): void {
-		if (token.isCancellationRequested) {
-			throw new Error('Operation cancelled');
-		}
-	}
-
-	export function normalizeNFC(items: string): string;
-	export function normalizeNFC(items: string[]): string[];
-	export function normalizeNFC(items: string | string[]): string | string[] {
-		if (process.platform !== 'darwin') {
-			return items;
-		}
-
-		if (Array.isArray(items)) {
-			return items.map(item => item.normalize('NFC'));
-		}
-
-		return items.normalize('NFC');
-	}
-
-	export function readdir(path: string): Promise<string[]> {
-		return new Promise<string[]>((resolve, reject) => {
-			fs.readdir(path, (error, children) => handleResult(resolve, reject, error, normalizeNFC(children)));
-		});
-	}
-
-	export function stat(path: string): Promise<fs.Stats> {
-		return new Promise<fs.Stats>((resolve, reject) => {
-			fs.stat(path, (error, stat) => handleResult(resolve, reject, error, stat));
-		});
-	}
-
-	export function readfile(path: string): Promise<Buffer> {
-		return new Promise<Buffer>((resolve, reject) => {
-			fs.readFile(path, (error, buffer) => handleResult(resolve, reject, error, buffer));
-		});
-	}
-
-	export function writefile(path: string, content: Buffer): Promise<void> {
-		return new Promise<void>((resolve, reject) => {
-			fs.writeFile(path, content, error => handleResult(resolve, reject, error, void 0));
-		});
-	}
-
-	export function exists(path: string): Promise<boolean> {
-		return new Promise<boolean>((resolve, reject) => {
-			fs.exists(path, exists => handleResult(resolve, reject, null, exists));
-		});
-	}
-
-	export function rmrf(path: string): Promise<void> {
-		return new Promise<void>((resolve, reject) => {
-			// rimrafSync(path, error => handleResult(resolve, reject, error, void 0));
-		});
-	}
-
-	export function mkdir(path: string): Promise<void> {
-		return new Promise<void>((resolve, reject) => {
-			// mkdirp(path, error => handleResult(resolve, reject, error, void 0));
-		});
-	}
-
-	export function rename(oldPath: string, newPath: string): Promise<void> {
-		return new Promise<void>((resolve, reject) => {
-			fs.rename(oldPath, newPath, error => handleResult(resolve, reject, error, void 0));
-		});
-	}
-
-	export function unlink(path: string): Promise<void> {
-		return new Promise<void>((resolve, reject) => {
-			fs.unlink(path, error => handleResult(resolve, reject, error, void 0));
-		});
-	}
-}
-
-export class FileStat implements vscode.FileStat {
-
-	constructor(private fsStat: fs.Stats) { }
-
-	get type(): vscode.FileType {
-		return this.fsStat.isFile() ? vscode.FileType.File : this.fsStat.isDirectory() ? vscode.FileType.Directory : this.fsStat.isSymbolicLink() ? vscode.FileType.SymbolicLink : vscode.FileType.Unknown;
-	}
-
-	get isFile(): boolean | undefined {
-		return this.fsStat.isFile();
-	}
-
-	get isDirectory(): boolean | undefined {
-		return this.fsStat.isDirectory();
-	}
-
-	get isSymbolicLink(): boolean | undefined {
-		return this.fsStat.isSymbolicLink();
-	}
-
-	get size(): number {
-		return this.fsStat.size;
-	}
-
-	get ctime(): number {
-		return this.fsStat.ctime.getTime();
-	}
-
-	get mtime(): number {
-		return this.fsStat.mtime.getTime();
-	}
-}
+import * as utils from './utilities';
 
 function getDirectories(dirPath: string, level = 0) {
 	let result: { path: string, level: number}[] = [];
@@ -159,21 +18,9 @@ function getDirectories(dirPath: string, level = 0) {
 	} catch (error) {
 	  console.error('Error reading directory:', error);
 	}
-  
+
 	return result;
 }
-
-interface Entry {
-	uri: vscode.Uri;
-	type: vscode.FileType;
-    id: number;
-    counter: {
-        value: number
-    };
-	parent: Entry | undefined;
-}
-
-//#endregion
 
 function numberToAlphabet(num: number) {
     const alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -206,60 +53,50 @@ function lettersToNumber(letters: string) {
     return num;
 }
 
+
+interface Entry {
+	uri: vscode.Uri;
+	type: vscode.FileType;
+    id: number;
+    counter: {
+        value: number
+    };
+	parent: Entry | undefined;
+}
+
 const randomNumbers: number[] = [];
-const uri_collapsibleState_map = new Map<string, vscode.TreeItemCollapsibleState>();
-const id_uri_map = new Map<number, string>();
-const uri_id_map = new Map<string, number>();
-const id_entry_map = new Map<number, Entry>(); 
+const uriCollapsibleStateMap = new Map<string, vscode.TreeItemCollapsibleState>();
+const idUriMap = new Map<number, string>();
+const uriIdMap = new Map<string, number>();
+const idEntryMap = new Map<number, Entry>();
 
-export class FileSystemProvider implements vscode.TreeDataProvider<Entry>, vscode.FileSystemProvider {
+export class FileSystemProvider implements vscode.TreeDataProvider<Entry> {
 
-	private _onDidChangeFile: vscode.EventEmitter<vscode.FileChangeEvent[]>;
-    _onDidChangeTreeData: vscode.EventEmitter<
-    Entry | undefined
-  > = new vscode.EventEmitter<Entry | undefined>();
-
-  readonly onDidChangeTreeData: vscode.Event<Entry | undefined> = this
-    ._onDidChangeTreeData.event;
+    private readonly _onDidChangeTreeData: vscode.EventEmitter<Entry | undefined> = new vscode.EventEmitter<Entry | undefined>();
+	readonly onDidChangeTreeData: vscode.Event<Entry | undefined> = this._onDidChangeTreeData.event;
 
 	constructor() {
-		this._onDidChangeFile = new vscode.EventEmitter<vscode.FileChangeEvent[]>();
         const workspaceFolder = (vscode.workspace.workspaceFolders ?? []).filter(folder => folder.uri.scheme === 'file')[0];
         if (workspaceFolder) {
             this.watch(workspaceFolder.uri, { recursive: true, excludes: [] });
         }
 	}
 
-	get onDidChangeFile(): vscode.Event<vscode.FileChangeEvent[]> {
-		return this._onDidChangeFile.event;
+	refresh(): void {
+		this._onDidChangeTreeData.fire(undefined);
 	}
 
 	watch(uri: vscode.Uri, options: { recursive: boolean; excludes: string[]; }): vscode.Disposable {
-		const watcher = fs.watch(uri.fsPath, { recursive: options.recursive }, async (event: string, filename: string | Buffer) => {
-			const filepath = path.join(uri.fsPath, _.normalizeNFC(filename.toString()));
+		const watcher = fs.watch(uri.fsPath, { recursive: options.recursive }, async () => {
 
-            id_uri_map.clear();
-			uri_id_map.clear();
-			id_entry_map.clear();
-			// TODO support excludes (using minimatch library?)
+            idUriMap.clear();
+			uriIdMap.clear();
+			idEntryMap.clear();
 
-			this._onDidChangeFile.fire([{
-				type: event === 'change' ? vscode.FileChangeType.Changed : await _.exists(filepath) ? vscode.FileChangeType.Created : vscode.FileChangeType.Deleted,
-				uri: uri.with({ path: filepath })
-			} as vscode.FileChangeEvent]);
-
-            this._onDidChangeTreeData.fire(undefined);
+            this.refresh();
 		});
 
 		return { dispose: () => watcher.close() };
-	}
-
-	stat(uri: vscode.Uri): vscode.FileStat | Thenable<vscode.FileStat> {
-		return this._stat(uri.fsPath);
-	}
-
-	async _stat(path: string): Promise<vscode.FileStat> {
-		return new FileStat(await _.stat(path));
 	}
 
 	readDirectory(uri: vscode.Uri): [string, vscode.FileType][] | Thenable<[string, vscode.FileType][]> {
@@ -267,75 +104,16 @@ export class FileSystemProvider implements vscode.TreeDataProvider<Entry>, vscod
 	}
 
 	async _readDirectory(uri: vscode.Uri): Promise<[string, vscode.FileType][]> {
-		const children = await _.readdir(uri.fsPath);
+		const children = await utils.readdir(uri.fsPath);
 
 		const result: [string, vscode.FileType][] = [];
 		for (let i = 0; i < children.length; i++) {
 			const child = children[i];
-			const stat = await this._stat(path.join(uri.fsPath, child));
+			const stat = new utils.FileStat(await utils.stat(path.join(uri.fsPath, child)));
 			result.push([child, stat.type]);
 		}
 
 		return Promise.resolve(result);
-	}
-
-	createDirectory(uri: vscode.Uri): void | Thenable<void> {
-		return _.mkdir(uri.fsPath);
-	}
-
-	readFile(uri: vscode.Uri): Uint8Array | Thenable<Uint8Array> {
-		return _.readfile(uri.fsPath);
-	}
-
-	writeFile(uri: vscode.Uri, content: Uint8Array, options: { create: boolean; overwrite: boolean; }): void | Thenable<void> {
-		return this._writeFile(uri, content, options);
-	}
-
-	async _writeFile(uri: vscode.Uri, content: Uint8Array, options: { create: boolean; overwrite: boolean; }): Promise<void> {
-		const exists = await _.exists(uri.fsPath);
-		if (!exists) {
-			if (!options.create) {
-				throw vscode.FileSystemError.FileNotFound();
-			}
-
-			await _.mkdir(path.dirname(uri.fsPath));
-		} else {
-			if (!options.overwrite) {
-				throw vscode.FileSystemError.FileExists();
-			}
-		}
-
-		return _.writefile(uri.fsPath, content as Buffer);
-	}
-
-	delete(uri: vscode.Uri, options: { recursive: boolean; }): void | Thenable<void> {
-		if (options.recursive) {
-			return _.rmrf(uri.fsPath);
-		}
-
-		return _.unlink(uri.fsPath);
-	}
-
-	rename(oldUri: vscode.Uri, newUri: vscode.Uri, options: { overwrite: boolean; }): void | Thenable<void> {
-		return this._rename(oldUri, newUri, options);
-	}
-
-	async _rename(oldUri: vscode.Uri, newUri: vscode.Uri, options: { overwrite: boolean; }): Promise<void> {
-		const exists = await _.exists(newUri.fsPath);
-		if (exists) {
-			if (!options.overwrite) {
-				throw vscode.FileSystemError.FileExists();
-			} else {
-				await _.rmrf(newUri.fsPath);
-			}
-		}
-
-		const parentExists = await _.exists(path.dirname(newUri.fsPath));
-		if (!parentExists) {
-			await _.mkdir(path.dirname(newUri.fsPath));
-		}
-
-		return _.rename(oldUri.fsPath, newUri.fsPath);
 	}
 
 	// tree data provider
@@ -348,8 +126,8 @@ export class FileSystemProvider implements vscode.TreeDataProvider<Entry>, vscod
             .map(([name, type]) => {
                 element.counter.value += 1;
                 const uri = vscode.Uri.file(path.join(element.uri.fsPath, name));
-                id_uri_map.set(element.counter.value, uri.path);
-				uri_id_map.set(uri.path, element.counter.value);
+                idUriMap.set(element.counter.value, uri.path);
+				uriIdMap.set(uri.path, element.counter.value);
                 return ({ uri, type, id: element.counter.value, counter: element.counter, parent: element });
             });
 		}
@@ -370,8 +148,8 @@ export class FileSystemProvider implements vscode.TreeDataProvider<Entry>, vscod
             .map(([name, type]) => {
                 counter.value += 1;
                 const uri = vscode.Uri.file(path.join(workspaceFolder.uri.fsPath, name));
-                id_uri_map.set(counter.value, uri.path);
-				uri_id_map.set(uri.path, counter.value);
+                idUriMap.set(counter.value, uri.path);
+				uriIdMap.set(uri.path, counter.value);
                 return ({ uri, type, id: counter.value, counter, parent: undefined });
             });
 		}
@@ -389,7 +167,7 @@ export class FileSystemProvider implements vscode.TreeDataProvider<Entry>, vscod
 			treeItem.command = { command: 'fileExplorer.openFile', title: "Open File", arguments: [element.uri], };
 			treeItem.contextValue = 'file';
 		} else {
-            const priorItem = uri_collapsibleState_map.get(element.uri.path);
+            const priorItem = uriCollapsibleStateMap.get(element.uri.path);
             if (priorItem) {
                 treeItem = new TreeItem(element.uri, priorItem, element.id, element);
 				let randomNumber;
@@ -408,8 +186,8 @@ export class FileSystemProvider implements vscode.TreeDataProvider<Entry>, vscod
 				treeItem.id = randomNumber.toString();
             }
         }
-        id_entry_map.set(element.id, element);
-        uri_collapsibleState_map.set(element.uri.path, treeItem.collapsibleState!);
+        idEntryMap.set(element.id, element);
+        uriCollapsibleStateMap.set(element.uri.path, treeItem.collapsibleState!);
 		return treeItem;
 	}
 }
@@ -457,11 +235,11 @@ export class FileExplorer {
 		if (itemId === undefined) {
 			return;
 		}
-        const uri = id_uri_map.get(itemId);
+        const uri = idUriMap.get(itemId);
         if (uri) {
-            const state = uri_collapsibleState_map.get(uri);
-            uri_collapsibleState_map.set(uri, state === vscode.TreeItemCollapsibleState.Collapsed ? vscode.TreeItemCollapsibleState.Expanded : vscode.TreeItemCollapsibleState.Collapsed)
-            this.treeDataProvider._onDidChangeTreeData.fire(undefined);
+            const state = uriCollapsibleStateMap.get(uri);
+            uriCollapsibleStateMap.set(uri, state === vscode.TreeItemCollapsibleState.Collapsed ? vscode.TreeItemCollapsibleState.Expanded : vscode.TreeItemCollapsibleState.Collapsed)
+            this.treeDataProvider.refresh();
         }
     }
 
@@ -470,20 +248,20 @@ export class FileExplorer {
 		if (itemId === undefined) {
 			return;
 		}
-        const uri = id_uri_map.get(itemId);
+        const uri = idUriMap.get(itemId);
 		const level = parseInt(levelString)
 		if (uri) {
-			if (uri_collapsibleState_map.get(uri) !== vscode.TreeItemCollapsibleState.Expanded) {
-				uri_collapsibleState_map.set(uri, vscode.TreeItemCollapsibleState.Expanded)
+			if (uriCollapsibleStateMap.get(uri) !== vscode.TreeItemCollapsibleState.Expanded) {
+				uriCollapsibleStateMap.set(uri, vscode.TreeItemCollapsibleState.Expanded)
 			}
 			for (const directory of getDirectories(uri)) {
 				if (directory.level >= level) {
-					uri_collapsibleState_map.set(directory.path, vscode.TreeItemCollapsibleState.Collapsed)
-				} else if (uri_collapsibleState_map.get(directory.path) !== vscode.TreeItemCollapsibleState.Expanded) {
-					uri_collapsibleState_map.set(directory.path, vscode.TreeItemCollapsibleState.Expanded)
+					uriCollapsibleStateMap.set(directory.path, vscode.TreeItemCollapsibleState.Collapsed)
+				} else if (uriCollapsibleStateMap.get(directory.path) !== vscode.TreeItemCollapsibleState.Expanded) {
+					uriCollapsibleStateMap.set(directory.path, vscode.TreeItemCollapsibleState.Expanded)
 				}
 			}
-			this.treeDataProvider._onDidChangeTreeData.fire(undefined);
+			this.treeDataProvider.refresh();
 		}
 	}
 
@@ -491,10 +269,10 @@ export class FileExplorer {
 		const workspacePath = vscode.workspace.workspaceFolders![0].uri.path;
 		for (const directory of getDirectories(workspacePath)) {
 			if (directory.level === 0) {
-				uri_collapsibleState_map.set(directory.path, vscode.TreeItemCollapsibleState.Collapsed)
+				uriCollapsibleStateMap.set(directory.path, vscode.TreeItemCollapsibleState.Collapsed)
 			}
 		}
-		this.treeDataProvider._onDidChangeTreeData.fire(undefined);
+		this.treeDataProvider.refresh();
 	}
 
 	private openFile(letters: string): void {
@@ -502,7 +280,7 @@ export class FileExplorer {
 		if (!itemId) {
 			return;
 		}
-		const uri = id_uri_map.get(itemId);
+		const uri = idUriMap.get(itemId);
 		if (uri) {
 			this.openResource(vscode.Uri.file(uri));
 		}
@@ -513,7 +291,7 @@ export class FileExplorer {
 		if (!itemId) {
 			return;
 		}
-		const uri = id_uri_map.get(itemId);
+		const uri = idUriMap.get(itemId);
 		if (uri) {
 			vscode.commands.executeCommand('fileutils.renameFile', vscode.Uri.file(uri));
 		}
@@ -524,7 +302,7 @@ export class FileExplorer {
 		if (!fromId) {
 			return;
 		}
-		const fromUri = id_uri_map.get(fromId);
+		const fromUri = idUriMap.get(fromId);
 		if (fromUri === undefined) {
 			return;
 		}
@@ -541,11 +319,11 @@ export class FileExplorer {
 			if (!toId) {
 				return;
 			}
-			const toUri = id_uri_map.get(toId);
+			const toUri = idUriMap.get(toId);
 			if (!toUri) {
 				return;
 			}
-			const isCollapsible = uri_collapsibleState_map.get(toUri)! !== vscode.TreeItemCollapsibleState.None;
+			const isCollapsible = uriCollapsibleStateMap.get(toUri)! !== vscode.TreeItemCollapsibleState.None;
 			let newUri;
 			if (isCollapsible) {
 				newUri = path.join(toUri, fileName);
@@ -561,9 +339,9 @@ export class FileExplorer {
 		if (!itemId) {
 			return;
 		}
-		const uri = id_uri_map.get(itemId);
+		const uri = idUriMap.get(itemId);
 		if (uri) {
-			const isCollapsible = uri_collapsibleState_map.get(uri)! !== vscode.TreeItemCollapsibleState.None;
+			const isCollapsible = uriCollapsibleStateMap.get(uri)! !== vscode.TreeItemCollapsibleState.None;
 			let directoryPath: string;
 			if (isCollapsible) {
 				directoryPath = uri;
@@ -580,8 +358,8 @@ export class FileExplorer {
 						const result = fileName.substring(0, fileName.length - 1);
 						let dirPath = path.join(directoryPath, result);
 						fs.mkdirSync(dirPath);
-						uri_collapsibleState_map.set(dirPath, vscode.TreeItemCollapsibleState.Expanded);
-						this.treeDataProvider._onDidChangeTreeData.fire(undefined);
+						uriCollapsibleStateMap.set(dirPath, vscode.TreeItemCollapsibleState.Expanded);
+						this.treeDataProvider.refresh();
 					}
 				}
 			})
@@ -593,7 +371,7 @@ export class FileExplorer {
 		if (!itemId) {
 			return;
 		}
-		const entry = id_entry_map.get(itemId)!;
+		const entry = idEntryMap.get(itemId)!;
 		this.treeView.reveal(entry, { focus: true });
 	}
 
@@ -602,7 +380,7 @@ export class FileExplorer {
 		if (!itemId) {
 			return;
 		}
-		const uri = id_uri_map.get(itemId);
+		const uri = idUriMap.get(itemId);
 		if (!uri) {
 			return;
 		}
@@ -613,16 +391,16 @@ export class FileExplorer {
 			'No'
 		).then((selection) => {
 			if (selection === 'Yes') {
-				const isCollapsible = uri_collapsibleState_map.get(uri)! !== vscode.TreeItemCollapsibleState.None;
+				const isCollapsible = uriCollapsibleStateMap.get(uri)! !== vscode.TreeItemCollapsibleState.None;
 
 				if (isCollapsible) {
 					fs.rmdirSync(uri, { recursive: true });
 				} else {
 					fs.unlinkSync(uri);
 				}
-				uri_collapsibleState_map.delete(uri);
-				this.treeDataProvider._onDidChangeTreeData.fire(undefined);
-			}				
+				uriCollapsibleStateMap.delete(uri);
+				this.treeDataProvider.refresh();
+			}
 		});
 	}
 }
---
+++ src/utilities.ts
@@ -0,0 +1,89 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+export class FileStat implements vscode.FileStat {
+
+	constructor(private fsStat: fs.Stats) { }
+
+	get type(): vscode.FileType {
+		return this.fsStat.isFile() ? vscode.FileType.File : this.fsStat.isDirectory() ? vscode.FileType.Directory : this.fsStat.isSymbolicLink() ? vscode.FileType.SymbolicLink : vscode.FileType.Unknown;
+	}
+
+	get isFile(): boolean | undefined {
+		return this.fsStat.isFile();
+	}
+
+	get isDirectory(): boolean | undefined {
+		return this.fsStat.isDirectory();
+	}
+
+	get isSymbolicLink(): boolean | undefined {
+		return this.fsStat.isSymbolicLink();
+	}
+
+	get size(): number {
+		return this.fsStat.size;
+	}
+
+	get ctime(): number {
+		return this.fsStat.ctime.getTime();
+	}
+
+	get mtime(): number {
+		return this.fsStat.mtime.getTime();
+	}
+}
+
+
+	function handleResult<T>(resolve: (result: T) => void, reject: (error: Error) => void, error: Error | null | undefined, result: T): void {
+		if (error) {
+			reject(massageError(error));
+		} else {
+			resolve(result);
+		}
+	}
+
+	function massageError(error: Error & { code?: string }): Error {
+		if (error.code === 'ENOENT') {
+			return vscode.FileSystemError.FileNotFound();
+		}
+
+		if (error.code === 'EISDIR') {
+			return vscode.FileSystemError.FileIsADirectory();
+		}
+
+		if (error.code === 'EEXIST') {
+			return vscode.FileSystemError.FileExists();
+		}
+
+		if (error.code === 'EPERM' || error.code === 'EACCESS') {
+			return vscode.FileSystemError.NoPermissions();
+		}
+
+		return error;
+	}
+
+	export function normalizeNFC(items: string[]): string[];
+	export function normalizeNFC(items: string | string[]): string | string[] {
+		if (process.platform !== 'darwin') {
+			return items;
+		}
+
+		if (Array.isArray(items)) {
+			return items.map(item => item.normalize('NFC'));
+		}
+
+		return items.normalize('NFC');
+	}
+
+	export function readdir(path: string): Promise<string[]> {
+		return new Promise<string[]>((resolve, reject) => {
+			fs.readdir(path, (error, children) => handleResult(resolve, reject, error, normalizeNFC(children)));
+		});
+	}
+
+	export function stat(path: string): Promise<fs.Stats> {
+		return new Promise<fs.Stats>((resolve, reject) => {
+			fs.stat(path, (error, stat) => handleResult(resolve, reject, error, stat));
+		});
+	}
