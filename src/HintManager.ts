7a242be134b6d830b3c8ab16d444274ca397c66e
David Tejada <davidxtejada@gmail.com>
2023-05-10 12:18:37 +0200

    Implement stable hints and more improvements to make it work like vscode's native file explorer

---
+++ src/HintManager.ts
@@ -0,0 +1,28 @@
+import { allHints } from "./allHints";
+import { debounce } from "lodash";
+
+export class HintManager {
+    private hints: string[];
+    private debouncedSort = debounce(this.sort, 100);
+
+    constructor() {
+        this.hints = [...allHints];
+    }
+
+    get() {
+        return this.hints.pop();
+    }
+
+    release(hint: string) {
+        this.hints.push(hint);
+        this.debouncedSort();
+    }
+
+    sort() {
+        this.hints.sort((a, b) => b.length - a.length || b.localeCompare(a));
+    }
+
+    reset() {
+        this.hints = [...allHints];
+    }
+}
--- src/fileUtils.ts
+++ src/fileUtils.ts
@@ -1,102 +1,40 @@
 import * as vscode from "vscode";
-import * as fs from "fs";
 
-export class FileStat implements vscode.FileStat {
-    constructor(private fsStat: fs.Stats) {}
-
-    get type(): vscode.FileType {
-        return this.fsStat.isFile()
-            ? vscode.FileType.File
-            : this.fsStat.isDirectory()
-            ? vscode.FileType.Directory
-            : this.fsStat.isSymbolicLink()
-            ? vscode.FileType.SymbolicLink
-            : vscode.FileType.Unknown;
-    }
-
-    get isFile(): boolean | undefined {
-        return this.fsStat.isFile();
-    }
-
-    get isDirectory(): boolean | undefined {
-        return this.fsStat.isDirectory();
-    }
-
-    get isSymbolicLink(): boolean | undefined {
-        return this.fsStat.isSymbolicLink();
-    }
-
-    get size(): number {
-        return this.fsStat.size;
-    }
-
-    get ctime(): number {
-        return this.fsStat.ctime.getTime();
-    }
-
-    get mtime(): number {
-        return this.fsStat.mtime.getTime();
+export async function exists(resource: vscode.Uri) {
+    try {
+        await vscode.workspace.fs.stat(resource);
+        return true;
+    } catch {
+        return false;
     }
 }
 
-function handleResult<T>(
-    resolve: (result: T) => void,
-    reject: (error: Error) => void,
-    error: Error | null | undefined,
-    result: T
-): void {
-    if (error) {
-        reject(massageError(error));
-    } else {
-        resolve(result);
-    }
-}
+export async function getDescendantFolders(
+    uri: vscode.Uri,
+    maxLevels: number
+): Promise<string[]> {
+    async function processFolder(
+        folderUri: vscode.Uri,
+        level: number
+    ): Promise<string[]> {
+        if (level > maxLevels) {
+            return [];
+        }
 
-function massageError(error: Error & { code?: string }): Error {
-    if (error.code === "ENOENT") {
-        return vscode.FileSystemError.FileNotFound();
-    }
+        const descendantFolders: string[] = [];
+        const entries = await vscode.workspace.fs.readDirectory(folderUri);
 
-    if (error.code === "EISDIR") {
-        return vscode.FileSystemError.FileIsADirectory();
-    }
+        const subfolderPromises = entries
+            .filter(([_, entryType]) => entryType === vscode.FileType.Directory)
+            .map(async ([entryName, _]) => {
+                const subFolderUri = vscode.Uri.joinPath(folderUri, entryName);
+                descendantFolders.push(subFolderUri.fsPath);
+                return processFolder(subFolderUri, level + 1);
+            });
 
-    if (error.code === "EEXIST") {
-        return vscode.FileSystemError.FileExists();
-    }
-
-    if (error.code === "EPERM" || error.code === "EACCESS") {
-        return vscode.FileSystemError.NoPermissions();
+        const subfolders = await Promise.all(subfolderPromises);
+        return descendantFolders.concat(...subfolders);
     }
 
-    return error;
-}
-
-export function normalizeNFC(items: string[]): string[];
-export function normalizeNFC(items: string | string[]): string | string[] {
-    if (process.platform !== "darwin") {
-        return items;
-    }
-
-    if (Array.isArray(items)) {
-        return items.map((item) => item.normalize("NFC"));
-    }
-
-    return items.normalize("NFC");
-}
-
-export function readdir(path: string): Promise<string[]> {
-    return new Promise<string[]>((resolve, reject) => {
-        fs.readdir(path, (error, children) =>
-            handleResult(resolve, reject, error, normalizeNFC(children))
-        );
-    });
-}
-
-export function stat(path: string): Promise<fs.Stats> {
-    return new Promise<fs.Stats>((resolve, reject) => {
-        fs.stat(path, (error, stat) =>
-            handleResult(resolve, reject, error, stat)
-        );
-    });
+    return processFolder(uri, 1);
 }
