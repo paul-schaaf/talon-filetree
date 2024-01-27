89ec1b6e3d1dabea4838a6a8df085cb0b145a2b2
David Tejada <davidxtejada@gmail.com>
2024-01-27 12:11:40 +0100

    Fix janky reveal of files when ancestor folders are already open

--- src/fileExplorer.ts
+++ src/fileExplorer.ts
@@ -688,7 +688,7 @@ export class FileExplorer {
             const entry = await this.treeDataProvider.getEntryFromPath(
                 directory
             );
-            if (entry) {
+            if (entry && entry.collapsibleState === 1) {
                 await this.treeView.reveal(entry, { select: false, expand: 1 });
             }
         }
